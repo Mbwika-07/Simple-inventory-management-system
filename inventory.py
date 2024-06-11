54ed806e468ff60a0084b8c012e0bcff6379b594
Mbwika-07 <allanmbwiks@gmail.com>
2024-06-11 15:43:49 +0300

    Finished on the add() and delete() function

--- inventory.py
+++ inventory.py
@@ -73,8 +73,34 @@ def update():
     else:
         print(f"\nThe item {product} is not available in the current inventory.")
 
+def add():
+    new_name = input("Enter new product name: ")
+    new_quantity = int(input("Enter the quantity you wish to add: "))
+    new_price = int(input("Enter price for the new product: "))
+
+    new_product = {
+        "price": new_price,
+        "quantity": new_quantity
+    }
+
+    products[new_name] = new_product
+    print(f"\nYou have successfully added the product {new_name} with the quantity {new_quantity} and price {new_price} to our inventory.")
+    print("\nHere is the updated product list with the new product: ")
+    
+    view()
+
 def delete():
-    pass
+    delete_product = input("Enter a product you would like to remove/delete: ").lower()
+    
+    if delete_product in products:
+        products.pop(delete_product)
+    else:
+        print("The product you typed is not in our inventory.")
+    
+    print(f"\nYou have successfully deleted the product {delete_product} from our inventory.")
+    print("\nHere is the updated product list.")
+    
+    view()
 
 
 is_running = True
@@ -82,9 +108,10 @@ while is_running:
     print("\n**********************************************************************************************************************************")
     print("Hello please choose one of the choices below to continue with the program.")
     print("\n\t1.View products")
-    print("\n\t2.Update products")
-    print("\n\t3.Delete products")
-    print("\n\t4.Exit program")
+    print("\n\t2.Update product details")
+    print("\n\t3.Add new product")
+    print("\n\t4.Delete product")
+    print("\n\t5.Exit program")
     choice = input("\nKindly enter your choice(1-4): ")
     print("\n**********************************************************************************************************************************")
     
@@ -93,8 +120,13 @@ while is_running:
     elif choice == "2":
         update()
     elif choice == "3":
-        delete()
+        add()
     elif choice == "4":
+        delete()
+    elif choice == "5":
         is_running = False
     else:
         print("Please enter a valid choice (1-4)")
+        
+
+
